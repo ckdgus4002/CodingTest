0acb49e01eb98cf2c778d12ead7927afb1c11571
LCH <ckdgus4002@gmail.com>
2021-04-10 03:40:08 +0900

    Commit.

---
+++ HackerRank/SockMerchant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingTest.HackerRank
+{
+    public class SockMerchant
+    {
+        public SockMerchant()
+        {
+        }
+    }
+}
