2d6be220135926e75ea18d8e879b2e2fa8766b66
LCH <ckdgus4002@gmail.com>
2021-05-06 19:35:28 +0900

    Commit.

--- Programmers/PrimeNumber/Program.cs
+++ Programmers/PrimeNumber/Program.cs
@@ -6,6 +6,8 @@ namespace LCH
     {
         public static void Main(string[] args)
         {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(new int[] { 1, 2, 3, 4 }));
         }
     }
 }
