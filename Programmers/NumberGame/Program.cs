ba8db06aa02ca0990cb89875015884764f380e68
LCH <ckdgus4002@gmail.com>
2021-05-08 19:21:54 +0900

    Commit.

--- Programmers/NumberGame/Program.cs
+++ Programmers/NumberGame/Program.cs
@@ -7,7 +7,7 @@ namespace NumberGame
         public static void Main(string[] args)
         {
             Solution solution = new Solution();
-            Console.WriteLine(solution.solution(new int[] { 5, 1, 3, 7 }, new int[] { 2, 2, 6, 8 }));
+            Console.WriteLine(solution.solution(new int[] { 5, 1, 3, 7 }, new int[] { 1, 3, 7, 5 }));
         }
     }
 }
--- Programmers/NumberGame/Solution.cs
+++ Programmers/NumberGame/Solution.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Linq;
 
 public class Solution
 {
     public int solution(int[] A, int[] B)
     {
+        //int bMax = B.Max();
+        //Array.Sort(B);
         int answer = 0;
+        for (int i = 0; i < A.Length; i++)
+        {
+            for (int j = 0; j < B.Length /*&& A[i] < bMax*/; j++)
+            {
+                if (0 < B[j] && A[i] < B[j])
+                {
+                    answer++;
+                    B[j] = 0;
+                    break;
+                }
+            }
+        }
+
         return answer;
     }
 }
