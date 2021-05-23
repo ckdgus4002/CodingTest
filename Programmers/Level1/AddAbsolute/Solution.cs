4dc415ed9bd519cd047eceaf3b827f159ff899c0
LCH <ckdgus4002@gmail.com>
2021-05-23 23:21:40 +0900

    Commit.

--- Programmers/Level1/AddAbsolute/Solution.cs
+++ Programmers/Level1/AddAbsolute/Solution.cs
@@ -4,7 +4,12 @@ public class Solution
 {
     public int solution(int[] absolutes, bool[] signs)
     {
-        int answer = 123456789;
+        int answer = 0;
+        for (int i = 0; i < absolutes.Length; i++)
+        {
+            answer += signs[i] ? absolutes[i] : -absolutes[i];
+        }
+
         return answer;
     }
 }
