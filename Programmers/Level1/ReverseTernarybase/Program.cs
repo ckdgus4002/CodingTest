cf87129be5ed4a5803db04ce2bdb6bc06b3181d0
LCH <ckdgus4002@gmail.com>
2021-05-26 21:28:45 +0900

    Commit.

---
+++ Programmers/Level1/ReverseTernarybase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LCH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Programmers/Level1/ReverseTernarybase/Solution.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution
+{
+    public int solution(int n) 
+    {
+        int answer = 0;
+        return answer;
+    }
+}
