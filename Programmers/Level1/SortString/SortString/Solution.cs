89ab1f5d168fc51f9e4b2f13dbf64f83b3f0ca53
LCH <ckdgus4002@gmail.com>
2021-06-05 01:07:12 +0900

    Commit.

--- Programmers/Level1/SortString/SortString/Solution.cs
+++ Programmers/Level1/SortString/SortString/Solution.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace SortString
+public class Solution
 {
-    class Program
+    private struct Item
     {
-        static void Main(string[] args)
+        public int index;
+        public string value;
+    } 
+    
+    public string[] solution(string[] strings, int n)
+    {
+        var items = new Item[strings.Length];
+        for (var i = 0; i < strings.Length; i++)
         {
-            Console.WriteLine("Hello World!");
+            items[i].index = i;
+            items[i].value = strings[i];
         }
+        
+        Array.Sort(items, (a,b) =>
+        {
+            if (a.value[n] < b.value[n]) return -1;
+            return a.value[n] == b.value[n] ? string.Compare(a.value, b.value, StringComparison.Ordinal) : 1;
+        });
+
+        var answer = new string[items.Length];
+        for (var i = 0; i < items.Length; i++) answer[i] = items[i].value;
+        
+        return answer;
     }
 }
