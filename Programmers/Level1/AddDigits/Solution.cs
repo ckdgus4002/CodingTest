7957364428e53d8b631585e0d1ecc19ecb455f2b
LCH <ckdgus4002@gmail.com>
2021-06-27 22:48:06 +0900

    Commit.

---
+++ Programmers/Level1/AddDigits/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Solution {
+    public long solution(long n)
+    {
+        var desc = n.ToString().Select(c => c - '0').OrderBy(a => a);
+        var index = 0;
+        long answer = 0;
+        foreach (var d in desc)
+        {
+            answer += d * (long)Math.Pow(10, index);
+            index++;
+        }
+        
+        return answer;
+    }
+}
---
+++ Programmers/Level1/CreateStrangeCharacters/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+public class Solution {
+    public string solution(string s) {
+        
+        var split = s.Split(' ');
+        var answer = new StringBuilder();
+        for (var i = 0; i < split.Length; i++)
+        {
+            for (var j = 0; j < split[i].Length; j++)
+            {
+                var append = split[i][j];
+                if (j % 2 == 0 && 'a' <= append) append = (char)(append - 32);
+                else if (j % 2 == 1 && append <= 'Z') append = (char) (append + 32);
+                
+                answer.Append(append);
+            }
+            
+            if (i < split.Length - 1) answer.Append(' ');
+        }
+        
+        return answer.ToString();
+    }
+}
