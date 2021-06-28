06f4bf9ba58927d0fca9747887000b2e747c17dc
LCH <ckdgus4002@gmail.com>
2021-06-29 00:33:53 +0900

    Commit GCD and LCM.

---
+++ Programmers/Level1/GCDAndLCM/Solution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+public class Solution {
+    public int[] solution(int n, int m)
+    {
+        var gcd = TakeGcd(n, m);
+        
+        return new[] { gcd, n * m / gcd };
+    }
+
+    private int TakeGcd(int n, int m) => n % m == 0 ? m : TakeGcd(m, n % m);   
+}
